a04fbc29c25e570a69e1f290ef9e57faeec44973
unknown <239017@TECH.WYB.AC.LK>
2025-03-26 19:41:12 +0530

    first commit

---
+++ Input.py
@@ -0,0 +1,4 @@
+num=int(input("Enter Your Number"))
+print(num)
+Name=str(input("Enter Your Name"))
+print(Name)
---
+++ pasi.py
@@ -0,0 +1,7 @@
+num1=int(input("Enter Your Number: "))
+print(num1)
+num2=int(input("Enter Your number: "))
+print(num2)
+sum=num1+num2
+div=sum/2
+print(div)
